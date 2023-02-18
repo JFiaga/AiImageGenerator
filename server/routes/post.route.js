ae34a934dd9d05baa49297d808c8e9171e334fec
Fiaga Fils <fiagafils@gmail.com>
2023-02-18 12:41:03 +0100

    link to cloudinary

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,13 +1,35 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import {v2 as cloudinary} from 'cloudinary'
+import express from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 
-import Post from '../db/models/post.model.js'
+import Post from "../db/models/post.model.js";
 
-dotenv.config()
+dotenv.config();
 
-const router = express.Router()
+const router = express.Router();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_API_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET,
+});
 
+//GET ALL POSTS
+router.route("/").get(async (req, res) => {
+  try {
+    const { name, photo, prompt } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
 
-export default router
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+
+    res.status(201).json({ success: true, data: newPost });
+  } catch (error) {
+    res.status(500).json({success:false, message:error.message})
+}
+});
+
+export default router;
